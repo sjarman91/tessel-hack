a4b484ebfddb28e66844434a4f1d5450298680ea
jazzyjackson <jazzyjackson@gmail.com>
2016-10-03 16:24:58 -0400

    says hello

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 var http = require('http');
-var slack = require('slack-incoming-webhook');
+var https = require('https');
+
+var options = {
+	host: 'hooks.slack.com',
+	path: '/services/T024FPYBQ/B2JPDA8KF/QaczRe2VAko0oGtotmY9JkVz',
+	port: '443',
+	method: 'POST'
+};
+
+
+var req = https.request(options);
+	
+req.write('{"text":"javascript says whats up"}');
+req.end();
 
